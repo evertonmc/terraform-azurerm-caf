de277ab7f227628259c608cdbb754900eb394e3b
Arnaud Lheureux <arnaudlh@hotmail.com>
2022-03-04 09:54:10 +0000

    Refactor CS

--- communication_services.tf
+++ communication_services.tf
@@ -5,8 +5,8 @@ module "communication_services" {
   global_settings     = local.global_settings
   client_config       = local.client_config
   settings            = each.value
-  resource_group_name = local.resource_groups[each.value.resource_group_key].name
-  base_tags           = try(local.global_settings.inherit_tags, false) ? local.resource_groups[each.value.resource_group_key].tags : {}
+  resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(local.client_config.landingzone_key, each.value.resource_group.lz_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
+  base_tags           = try(local.global_settings.inherit_tags, false) ? local.combined_objects_resource_groups[try(local.client_config.landingzone_key, each.value.resource_group.lz_key)][try(each.value.resource_group.key, each.value.resource_group_key)].tags : {}
 }
 
 output "communication_services" {
